d1c2da30c2fc9efe5cc91a665df65ca46aa8abb3
Wonhyeonsik <shawnpenn198@gmail.com>
2024-04-08 09:57:13 +0900

    First Commit

---
+++ src/main/java/net/kdigital/ccaTry/SourceTreeApplication.java
@@ -0,0 +1,13 @@
+package net.kdigital.ccaTry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kdigital/ccaTry/SourceTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kdigital.ccaTry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
