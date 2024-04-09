40a25fe1aa78198753f89b94d820c23c4149721f
Wonhyeonsik <shawnpenn198@gmail.com>
2024-04-09 11:40:25 +0900

    브랜치 테스트 하기위해 메인 만듬

---
+++ src/main/java/net/kdigital/BranchTest/BranchTestApplication.java
@@ -0,0 +1,13 @@
+package net.kdigital.BranchTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kdigital/BranchTest/BranchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kdigital.BranchTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
